e6f68c7098a6ccc700c012cc697f7c7e00eabe2f
Evgeniy KLivets <98554759+EvgeniyKlivets@users.noreply.github.com>
2022-08-10 23:37:36 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.css',
+                'resources/sass/app.scss',
                 'resources/js/app.js',
                 'resources/js/images-preview.js',
                 'resources/js/images-actions.js',
