4e0b3006880bbd292bb1db96cfc50f2be2ab2314
Evgeniy KLivets <98554759+EvgeniyKlivets@users.noreply.github.com>
2022-08-15 22:47:45 +0300

    Add files via upload

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -8,38 +8,37 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
-@ -17,27 +19,36 @@
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
      */
     public function definition()
     {
         $role = Role::customer()->first();
+
         return [
             'role_id' => $role->id,
             'name' => fake()->name(),
-            'email' => fake()->safeEmail(),
             'surname' => fake()->lastName,
             'birthdate' => fake()->dateTimeBetween('-70 years', '-18 years')->format('Y-m-d'),
             'email' => fake()->unique()->safeEmail(),
             'phone' => fake()->unique()->e164PhoneNumber,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'password' => Hash::make('test1234'),
             'remember_token' => Str::random(10),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
     public function admin()
     {
         return $this->state(function (array $attributes) {
             return [
-                'email_verified_at' => null,
-                'role_id'=>Role::admin()->first()->id,
+                'role_id' => Role::admin()->first()->id,
             ];
         });
     }
@@ -52,5 +51,13 @@ use Illuminate\Support\Str;
             ];
         });
     }
-}
 
+    public function withPassword(string $password)
+    {
+        return $this->state(function (array $attributes) use ($password) {
+            return [
+                'password' => Hash::make($password)
+            ];
+        });
+    }
+}
