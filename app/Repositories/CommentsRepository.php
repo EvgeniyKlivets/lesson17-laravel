54f82f37870d3f0b117a6ba49894c6ce4f6b82ab
Evgeniy KLivets <98554759+EvgeniyKlivets@users.noreply.github.com>
2022-09-11 20:01:21 +0300

    Create CommentsRepository.php

---
+++ app/Repositories/CommentsRepository.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Repositories;
+
+use App\Models\Comment;
+use App\Repositories\Contracts\CommentsRepositoryContract;
+
+class CommentsRepository implements CommentsRepositoryContract
+{
+
+    public function create(Comment $comment, string $model, int $id): Comment|bool
+    {
+        if (!class_exists($model)) {
+            throw new \Exception("{$model} does not exists");
+        }
+
+//        $model = new $model;
+//        $model = $model->find($id);
+
+        $model = app()->make($model)->find($id);
+
+        return $model->comments()->save($comment);
+    }
+}
